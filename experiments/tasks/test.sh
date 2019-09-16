cb20d5de5455ad8e45fc0259e71583ffc302f857
Irbe Krumina <irbekrm@gmail.com>
2019-09-16 19:38:37 +0100

    Add a parmeterised pipeline

---
+++ experiments/tasks/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+
+vault_auth="\"role_id\":\"${ROLE_ID}\",\"secret_id\":\"${SECRET_ID}\""
+
+result=$(curl \
+  --request POST 
+  --data "{${vault_auth}}" \
+  "https://${VAULT_URL}/v1/auth/approle/login")
+
+echo $result
