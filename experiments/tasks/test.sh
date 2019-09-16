4a01714d1e86d8ed32e93572dcaacda486cb88dd
Irbe Krumina <irbekrm@gmail.com>
2019-09-16 19:42:50 +0100

    corrects curl

--- experiments/tasks/test.sh
+++ experiments/tasks/test.sh
@@ -5,7 +5,7 @@ set -x
 vault_auth="\"role_id\":\"${ROLE_ID}\",\"secret_id\":\"${SECRET_ID}\""
 
 result=$(curl \
-  --request POST 
+  --request POST \
   --data "{${vault_auth}}" \
   "https://${VAULT_URL}/v1/auth/approle/login")
 
