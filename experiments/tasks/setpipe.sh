a36ddad3463ed87dfe377c25db77aa3e1756a253
Irbe Krumina <irbekrm@gmail.com>
2019-10-01 21:32:00 +0100

    Adds a second job to set-pipeline pipe

--- experiments/tasks/setpipe.sh
+++ experiments/tasks/setpipe.sh
@@ -5,4 +5,6 @@ set -x
 
 fly -t conc login -c "${CONCOURSE_URL}" -u "${CONCOURSE_USER}" -p "${CONCOURSE_PASSWORD}"
 
+fly -t conc sync
+
 fly -t conc set-pipeline -p "${PIPELINE_NAME}" -c learning-concourse/experiments/setpipe.yaml --non-interactive
