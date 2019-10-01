304978b64e695948bc4b423b2fff089b1cdb8bdf
Irbe Krumina <irbekrm@gmail.com>
2019-10-01 21:25:35 +0100

    Set pipeline non-interactively

--- experiments/tasks/setpipe.sh
+++ experiments/tasks/setpipe.sh
@@ -5,4 +5,4 @@ set -x
 
 fly -t conc login -c "${CONCOURSE_URL}" -u "${CONCOURSE_USER}" -p "${CONCOURSE_PASSWORD}"
 
-fly -t conc set-pipeline -p "${PIPELINE_NAME}" -c learning-concourse/experiments/tasks/setpipe.yaml
+fly -t conc set-pipeline -p "${PIPELINE_NAME}" -c learning-concourse/experiments/setpipe.yaml --non-interactive
