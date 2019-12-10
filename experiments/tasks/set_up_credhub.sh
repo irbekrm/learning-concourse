c9f18295fef8ad8500b451d726a9464780919095
Irbe Krumina <irbekrm@gmail.com>
2019-12-10 07:29:16 +0000

    Runs set_up_credhub as a privileged task

--- experiments/tasks/set_up_credhub.sh
+++ experiments/tasks/set_up_credhub.sh
@@ -6,17 +6,8 @@ source /docker-lib.sh
 
 start_docker
 
-docker load -i credhub_image
-docker tag "$(cat credhub_image/image-id)" "$(cat credhub_image/repository):$(cat credhub_image/tag)"
-
-docker load -i uaa_image
-docker tag "$(cat uaa_image/image-id)" "$(cat uaa_image/repository):$(cat uaa_image/tag)"
-
-# Check that images have been loaded
-docker images
-
 cd git_repo/experiments/files
-docker-compose -f docker-compose.yml -d
+docker-compose up -d
 
 credhub login -s https://localhost:9000 -u credhub -p password --skip-tls-validation
 credhub set - n /concourse/main/foo -t value -v bar
